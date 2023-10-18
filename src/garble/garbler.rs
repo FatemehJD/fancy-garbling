61fd813df80099bec558c81fe9cd6d14b8571a98
FatemehJD <135868927+FatemehJD@users.noreply.github.com>
2023-10-17 23:44:02 -0500

    Update garbler.rs

--- src/garble/garbler.rs
+++ src/garble/garbler.rs
@@ -350,8 +350,9 @@ impl<C: AbstractChannel, RNG: CryptoRng + RngCore> Fancy for Garbler<C, RNG> {
             println!("count: {:?}, garbler.rs send block: {:?}", count, block);
             count = count + 1;
         }
-        println!("before sending");
-        self.channel.write_bool(false)?;
+        let temp = false;
+        println!("before sending {:?}", temp);
+        self.channel.write_bool(temp)?;
         println!("after sending");
         Ok(())
     }
